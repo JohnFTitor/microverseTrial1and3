abb9607aac0c3a087a966e250c78d2b62eeb5881
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2021-09-29 12:03:18 -0500

    Removing mistaken commit
    I apologize. I was working in the other Microverse Trial and accidentally
    commited this project which was already completed. Please ignore this last two
    commits

--- js/index.js
+++ js/index.js
@@ -26,13 +26,3 @@ INSTRUCTIONS:
 
 // Once you have read the above messages, you can delete all comments. 
 "use strict";
-
-function designerPdfViewer(h, word) {
-  // Write your code here
-  let L = ["a", "b", "c","d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  let wordArray = word.split("");
-  let index = wordArray.map((i)=>{
-    return L.indexOf(i);
-  })
-  console.log(index);
-} 
